cdb43b9a294b924b28b9f8bf9781d7545189d5cf
Slawomir Demichowicz <slawomir.demichowicz@gmail.com>
2013-07-05 20:37:45 +0100

    Reformatted file.

--- dyna/javascripts/Character.js
+++ dyna/javascripts/Character.js
@@ -1,43 +1,45 @@
-/*
- * Only rendering and diplay 
- */ 
+define('Character', function(){
+  /*
+   * Only rendering and diplay
+   */
 
-var Character = function(spriteImage){
-    /* configuration */ 
-    this._sprite_animation_frequency = 10;    
+  var Character = function(spriteImage){
+    /* configuration */
+    this._sprite_animation_frequency = 10;
 
-    /* initialization */ 
+    /* initialization */
     this._spriteImage = spriteImage;
 
-    /* construction */ 
+    /* construction */
     this._spriteSheet = new createjs.SpriteSheet(this._getSpriteSheetProperties());
     this.animation    = new createjs.BitmapAnimation(this._spriteSheet);
-}
+  };
 
-Character.prototype._getSpriteSheetProperties = function(){
+  Character.prototype._getSpriteSheetProperties = function(){
     return {
-        images: [this._spriteImage],
-        frames: {
-            width: 32,
-            height: 48
+      images: [this._spriteImage],
+      frames: {
+        width: 32,
+        height: 48
+      },
+      animations: {
+        walkDown: {
+          frames: [0, 1, 2, 3],
+          frequency: this._sprite_animation_frequency
         },
-        animations: {
-            walkDown: { 
-                frames: [0, 1, 2, 3], 
-                frequency: this._sprite_animation_frequency
-            }, 
-            walkLeft: {
-                frames: [4, 5, 6, 7], 
-                frequency: this._sprite_animation_frequency
-            },
-            walkRight: {
-                frames: [8, 9, 10, 11], 
-                frequency: this._sprite_animation_frequency
-            },
-            walkUp: {
-                frames: [12, 13, 14, 15], 
-                frequency: this._sprite_animation_frequency
-            }
+        walkLeft: {
+          frames: [4, 5, 6, 7],
+          frequency: this._sprite_animation_frequency
+        },
+        walkRight: {
+          frames: [8, 9, 10, 11],
+          frequency: this._sprite_animation_frequency
+        },
+        walkUp: {
+          frames: [12, 13, 14, 15],
+          frequency: this._sprite_animation_frequency
         }
+      }
     };
-}
+  }
+})
